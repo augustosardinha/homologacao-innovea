7d55981387be01a27f723aaf6d00ed7ae82fe060
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2023-07-13 17:51:58 -0300

    feat: dark and light theme

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,17 @@
+<script setup lang="ts">
+import ToggleTheme from './ToggleTheme.vue'
+</script>
+
 <template>
-  <header class="w-full flex justify-center bg-zinc-950 py-3 text-white">
+  <header class="w-full flex justify-between items-center bg-zinc-950 py-3 text-white px-6">
     <a
       href="https://newsapi.org/"
-      class="text-2xl md:text-3xl font-bold cursor-pointer bg-opacity-70 hover:bg-gray-900 p-3 rounded"
+      class="text-center text-2xl md:text-3xl font-bold cursor-pointer bg-opacity-70 hover:bg-gray-900 p-3 rounded"
       target="_blank"
     >
-      NewsAPI
+      News
     </a>
+
+    <ToggleTheme />
   </header>
 </template>
---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import SunIcon from '@/assets/icons/SunIcon.vue'
+import MoonIcon from '@/assets/icons/MoonIcon.vue'
+
+const isDark = useDark()
+const toggleTheme = useToggle(isDark)
+</script>
+
+<template>
+  <button @click="toggleTheme()">
+    <Transition name="slide-fade" mode="out-in" :duration="150">
+      <component :is="isDark ? MoonIcon : SunIcon" />
+    </Transition>
+  </button>
+</template>
